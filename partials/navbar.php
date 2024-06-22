92e175e57dab7c0d7269998a5f0e2b674c8fc2fe
mhmd-iqbal <mhmd.iqbal1599@gmail.com>
2024-06-22 17:29:36 +0700

    update navbar: - ukuran text logo dibuat lebih kecil - ukuran button dibuat lebih kecil - height navbar dikurangi menjadi 80px

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,10 +1,12 @@
-<nav class="navbar navbar-expand-lg bg-body shadow-sm px-4" style="height: 100px; border-radius: 20px;">
-    <h5 class="mb-0" style="letter-spacing: 1.4px;"><i class="bi bi-list-task me-2"></i>Task Manager App</h5>
-    <div class="ms-auto">
+<nav class="navbar navbar-expand-lg bg-body shadow-sm px-4" style="height: 80px; border-radius: 20px;">
+    <div class="d-flex flex-wrap w-100 align-items-center justify-content-between gap-2">
+        <div class="me-4">
+            <h6 class="mb-0" style="letter-spacing: 1.4px;"><i class="bi bi-list-task me-2"></i>Task Manager App</h6>
+        </div>
         <div class="d-flex gap-2">
-            <a href="https://www.github.com/mhmd-iqbal" target="_blank" class="btn btn-outline-dark rounded-circle d-flex align-items-center justify-content-center" style="height: 40px; width: 40px;"><i class="bi bi-github"></i></a>
-            <a href="https://www.instagram.com/mhmd.iqbaale_" target="_blank" class="btn btn-outline-danger rounded-circle d-flex align-items-center justify-content-center" style="height: 40px; width: 40px;"><i class="bi bi-instagram"></i></a>
-            <a href="https://www.linkedin.com/in/wd-mhmd-iqbal" target="_blank" class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center" style="height: 40px; width: 40px;"><i class="bi bi-linkedin"></i></a>
+            <a href="https://www.github.com/mhmd-iqbal" target="_blank" class="btn btn-outline-dark rounded-circle d-flex align-items-center justify-content-center" style="height: 30px; width: 30px;"><i style="font-size: 10pt" class="bi bi-github"></i></a>
+            <a href="https://www.instagram.com/mhmd.iqbaale_" target="_blank" class="btn btn-outline-danger rounded-circle d-flex align-items-center justify-content-center" style="height: 30px; width: 30px;"><i style="font-size: 10pt" class="bi bi-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/wd-mhmd-iqbal" target="_blank" class="btn btn-outline-primary rounded-circle d-flex align-items-center justify-content-center" style="height: 30px; width: 30px;"><i style="font-size: 10pt" class="bi bi-linkedin"></i></a>
         </div>
     </div>
 </nav>
